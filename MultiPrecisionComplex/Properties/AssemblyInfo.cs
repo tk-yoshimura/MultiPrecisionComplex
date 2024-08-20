0d5fea5a21914547aae5f0169e1d1561d152d2d1
tk-yoshimura <yoshimura8192@gmail.com>
2024-08-20 14:37:12 +0900

    Update AssemblyInfo.cs

--- MultiPrecisionComplex/Properties/AssemblyInfo.cs
+++ MultiPrecisionComplex/Properties/AssemblyInfo.cs
@@ -15,6 +15,6 @@ using System.Runtime.InteropServices;
 
 [assembly: Guid("E71B55EC-7E6D-4735-B7E1-5477CFFEAD09")]
 
-[assembly: AssemblyVersion("1.1.4.*")]
+[assembly: AssemblyVersion("1.1.5.*")]
 
 [assembly: InternalsVisibleTo("MultiPrecisionComplexTest")]
