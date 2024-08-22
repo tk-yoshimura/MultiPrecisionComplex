63c9ed83265203cb6afc32e4d8a0fa82bb0484bd
tk-yoshimura <yoshimura8192@gmail.com>
2024-08-23 02:27:02 +0900

    cleanup

--- MultiPrecisionComplexTest/Complex/ComplexArithmeticTests.cs
+++ MultiPrecisionComplexTest/Complex/ComplexArithmeticTests.cs
@@ -128,7 +128,7 @@ namespace MultiPrecisionComplexTests {
             }
         }
 
-        
+
         [TestMethod()]
         public void InverseTest() {
             foreach (Complex<Pow2.N8> z in new[] { (1, 2), (2, 5), (6, -3), (7, -4), (3, -9), (7, 1), (-3, -4), (-1, -9), (-2, 1) }) {
