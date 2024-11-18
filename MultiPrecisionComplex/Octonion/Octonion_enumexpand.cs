7c425bfee0b038fa00f6894da8e8525d23ce9fcb
tk-yoshimura <yoshimura8192@gmail.com>
2024-11-18 23:38:48 +0900

    add: octonion enum

---
+++ MultiPrecisionComplex/Octonion/Octonion_enumexpand.cs
@@ -0,0 +1,102 @@
+﻿using MultiPrecision;
+
+namespace MultiPrecisionComplex {
+
+    public static class OctonionEnumerableExpand {
+        public static Octonion<N> Sum<N>(this IEnumerable<Octonion<N>> source) where N : struct, IConstant {
+            Octonion<N> acc = Octonion<N>.Zero, carry = Octonion<N>.Zero;
+
+            foreach (var v in source) {
+                Octonion<N> d = v - carry;
+                Octonion<N> acc_next = acc + d;
+
+                carry = (acc_next - acc) - d;
+                acc = acc_next;
+            }
+
+            return acc;
+        }
+
+        public static Octonion<N> Average<N>(this IEnumerable<Octonion<N>> source) where N : struct, IConstant {
+            return source.Sum() / source.Count();
+        }
+
+        public static IEnumerable<MultiPrecision<N>> R<N>(this IEnumerable<Octonion<N>> source) where N : struct, IConstant {
+            foreach (var v in source) {
+                yield return v.R;
+            }
+        }
+
+        public static IEnumerable<MultiPrecision<N>> I<N>(this IEnumerable<Octonion<N>> source) where N : struct, IConstant {
+            foreach (var v in source) {
+                yield return v.I;
+            }
+        }
+
+        public static IEnumerable<MultiPrecision<N>> J<N>(this IEnumerable<Octonion<N>> source) where N : struct, IConstant {
+            foreach (var v in source) {
+                yield return v.J;
+            }
+        }
+
+        public static IEnumerable<MultiPrecision<N>> K<N>(this IEnumerable<Octonion<N>> source) where N : struct, IConstant {
+            foreach (var v in source) {
+                yield return v.K;
+            }
+        }
+
+        public static IEnumerable<MultiPrecision<N>> W<N>(this IEnumerable<Octonion<N>> source) where N : struct, IConstant {
+            foreach (var v in source) {
+                yield return v.W;
+            }
+        }
+
+        public static IEnumerable<MultiPrecision<N>> X<N>(this IEnumerable<Octonion<N>> source) where N : struct, IConstant {
+            foreach (var v in source) {
+                yield return v.X;
+            }
+        }
+
+        public static IEnumerable<MultiPrecision<N>> Y<N>(this IEnumerable<Octonion<N>> source) where N : struct, IConstant {
+            foreach (var v in source) {
+                yield return v.Y;
+            }
+        }
+
+        public static IEnumerable<MultiPrecision<N>> Z<N>(this IEnumerable<Octonion<N>> source) where N : struct, IConstant {
+            foreach (var v in source) {
+                yield return v.Z;
+            }
+        }
+
+        public static IEnumerable<MultiPrecision<N>> SquareNorm<N>(this IEnumerable<Octonion<N>> source) where N : struct, IConstant {
+            foreach (var v in source) {
+                yield return v.SquareNorm;
+            }
+        }
+
+        public static IEnumerable<MultiPrecision<N>> Norm<N>(this IEnumerable<Octonion<N>> source) where N : struct, IConstant {
+            foreach (var v in source) {
+                yield return v.Norm;
+            }
+        }
+
+        public static IEnumerable<MultiPrecision<N>> Magnitude<N>(this IEnumerable<Octonion<N>> source) where N : struct, IConstant {
+            foreach (var v in source) {
+                yield return v.Magnitude;
+            }
+        }
+
+        public static IEnumerable<Octonion<N>> Conjugate<N>(this IEnumerable<Octonion<N>> source) where N : struct, IConstant {
+            foreach (var v in source) {
+                yield return v.Conj;
+            }
+        }
+
+        public static IEnumerable<Octonion<N>> Normal<N>(this IEnumerable<Octonion<N>> source) where N : struct, IConstant {
+            foreach (var v in source) {
+                yield return v.Normal;
+            }
+        }
+    }
+}
---
+++ MultiPrecisionComplexTest/Octonion/OctonionEnumTests.cs
@@ -0,0 +1,106 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MultiPrecision;
+using MultiPrecisionComplex;
+
+namespace MultiPrecisionComplexTests {
+    [TestClass()]
+    public class OctonionEnumTests {
+        [TestMethod()]
+        public void OctonionSumTest() {
+            Octonion<Pow2.N4>[] qs = { (1, 2, 3, 4, 2, 5, 7, 8), (2, 5, -2, 6, 3, 2, 4, 6), (6, -3, 1, 2, 7, 1, 5, 6), (7, -3, 5, 1, -4, -2, 1, 7) };
+
+            Assert.AreEqual((16, 1, 7, 13, 8, 6, 17, 27), qs.Sum());
+        }
+
+        [TestMethod()]
+        public void OctonionAverageTest() {
+            Octonion<Pow2.N4>[] qs = { (1, 2, 3, 4, 2, 5, 7, 8), (2, 5, -2, 6, 3, 2, 4, 6), (6, -3, 1, 2, 7, 1, 5, 6), (7, -3, 5, 1, -4, -2, 1, 7) };
+
+            Assert.AreEqual((4, 0.25, 1.75, 3.25, 2, 1.5, 4.25, 6.75), qs.Average());
+        }
+
+        [TestMethod()]
+        public void OctonionRTest() {
+            Octonion<Pow2.N4>[] qs = { (1, 2, 3, 4, 2, 5, 7, 8), (2, 5, -2, 6, 3, 2, 4, 6), (6, -3, 1, 2, 7, 1, 5, 6), (7, -3, 5, 1, -4, -2, 1, 7) };
+
+            CollectionAssert.AreEqual(new MultiPrecision<Pow2.N4>[] { 1, 2, 6, 7 }, qs.R().ToArray());
+        }
+
+        [TestMethod()]
+        public void OctonionITest() {
+            Octonion<Pow2.N4>[] qs = { (1, 2, 3, 4, 2, 5, 7, 8), (2, 5, -2, 6, 3, 2, 4, 6), (6, -3, 1, 2, 7, 1, 5, 6), (7, -3, 5, 1, -4, -2, 1, 7) };
+
+            CollectionAssert.AreEqual(new MultiPrecision<Pow2.N4>[] { 2, 5, -3, -3 }, qs.I().ToArray());
+        }
+
+        [TestMethod()]
+        public void OctonionJTest() {
+            Octonion<Pow2.N4>[] qs = { (1, 2, 3, 4, 2, 5, 7, 8), (2, 5, -2, 6, 3, 2, 4, 6), (6, -3, 1, 2, 7, 1, 5, 6), (7, -3, 5, 1, -4, -2, 1, 7) };
+
+            CollectionAssert.AreEqual(new MultiPrecision<Pow2.N4>[] { 3, -2, 1, 5 }, qs.J().ToArray());
+        }
+
+        [TestMethod()]
+        public void OctonionKTest() {
+            Octonion<Pow2.N4>[] qs = { (1, 2, 3, 4, 2, 5, 7, 8), (2, 5, -2, 6, 3, 2, 4, 6), (6, -3, 1, 2, 7, 1, 5, 6), (7, -3, 5, 1, -4, -2, 1, 7) };
+
+            CollectionAssert.AreEqual(new MultiPrecision<Pow2.N4>[] { 4, 6, 2, 1 }, qs.K().ToArray());
+        }
+
+        [TestMethod()]
+        public void OctonionWTest() {
+            Octonion<Pow2.N4>[] qs = { (1, 2, 3, 4, 2, 5, 7, 8), (2, 5, -2, 6, 3, 2, 4, 6), (6, -3, 1, 2, 7, 1, 5, 6), (7, -3, 5, 1, -4, -2, 1, 7) };
+
+            CollectionAssert.AreEqual(new MultiPrecision<Pow2.N4>[] { 2, 3, 7, -4 }, qs.W().ToArray());
+        }
+
+        [TestMethod()]
+        public void OctonionXTest() {
+            Octonion<Pow2.N4>[] qs = { (1, 2, 3, 4, 2, 5, 7, 8), (2, 5, -2, 6, 3, 2, 4, 6), (6, -3, 1, 2, 7, 1, 5, 6), (7, -3, 5, 1, -4, -2, 1, 7) };
+
+            CollectionAssert.AreEqual(new MultiPrecision<Pow2.N4>[] { 5, 2, 1, -2 }, qs.X().ToArray());
+        }
+
+        [TestMethod()]
+        public void OctonionYTest() {
+            Octonion<Pow2.N4>[] qs = { (1, 2, 3, 4, 2, 5, 7, 8), (2, 5, -2, 6, 3, 2, 4, 6), (6, -3, 1, 2, 7, 1, 5, 6), (7, -3, 5, 1, -4, -2, 1, 7) };
+
+            CollectionAssert.AreEqual(new MultiPrecision<Pow2.N4>[] { 7, 4, 5, 1 }, qs.Y().ToArray());
+        }
+
+        [TestMethod()]
+        public void OctonionZTest() {
+            Octonion<Pow2.N4>[] qs = { (1, 2, 3, 4, 2, 5, 7, 8), (2, 5, -2, 6, 3, 2, 4, 6), (6, -3, 1, 2, 7, 1, 5, 6), (7, -3, 5, 1, -4, -2, 1, 7) };
+
+            CollectionAssert.AreEqual(new MultiPrecision<Pow2.N4>[] { 8, 6, 6, 7 }, qs.Z().ToArray());
+        }
+
+        [TestMethod()]
+        public void OctonionNormTest() {
+            Octonion<Pow2.N4>[] qs = { (1, 2, 3, 4, 2, 5, 7, 8), (2, 5, -2, 6, 3, 2, 4, 6), (6, -3, 1, 2, 7, 1, 5, 6), (7, -3, 5, 1, -4, -2, 1, 7) };
+
+            CollectionAssert.AreEqual(new MultiPrecision<Pow2.N4>[] { qs[0].Norm, qs[1].Norm, qs[2].Norm, qs[3].Norm }, qs.Norm().ToArray());
+        }
+
+        [TestMethod()]
+        public void OctonionMagnitudeTest() {
+            Octonion<Pow2.N4>[] qs = { (1, 2, 3, 4, 2, 5, 7, 8), (2, 5, -2, 6, 3, 2, 4, 6), (6, -3, 1, 2, 7, 1, 5, 6), (7, -3, 5, 1, -4, -2, 1, 7) };
+
+            CollectionAssert.AreEqual(new MultiPrecision<Pow2.N4>[] { qs[0].Norm, qs[1].Norm, qs[2].Norm, qs[3].Norm }, qs.Magnitude().ToArray());
+        }
+
+        [TestMethod()]
+        public void OctonionConjugateTest() {
+            Octonion<Pow2.N4>[] qs = { (1, 2, 3, 4, 2, 5, 7, 8), (2, 5, -2, 6, 3, 2, 4, 6), (6, -3, 1, 2, 7, 1, 5, 6), (7, -3, 5, 1, -4, -2, 1, 7) };
+
+            CollectionAssert.AreEqual(new Octonion<Pow2.N4>[] { qs[0].Conj, qs[1].Conj, qs[2].Conj, qs[3].Conj }, qs.Conjugate().ToArray());
+        }
+
+        [TestMethod()]
+        public void OctonionNormalTest() {
+            Octonion<Pow2.N4>[] qs = { (1, 2, 3, 4, 2, 5, 7, 8), (2, 5, -2, 6, 3, 2, 4, 6), (6, -3, 1, 2, 7, 1, 5, 6), (7, -3, 5, 1, -4, -2, 1, 7) };
+
+            CollectionAssert.AreEqual(new Octonion<Pow2.N4>[] { qs[0].Normal, qs[1].Normal, qs[2].Normal, qs[3].Normal }, qs.Normal().ToArray());
+        }
+    }
+}
